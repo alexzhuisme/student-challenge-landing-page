8e94ef8d91f17fff7e491a221e8686a883314d75
Junyu Zhu <alexzhuisme@gmail.com>
2021-06-23 00:23:50 -0700

    update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,6 @@
 export default function Banner() {
   return (
-    <div className="w-full h-auto bg-gradient-to-r from-orange-dark to-orange-light">
-      <p className="pl-6 md:w-1/2 py-2 h-full text-base text-white flex justify-start mx-auto sm:w-full">
-        <span className="font-bold">Foxit</span>&nbsp;has changed some product names. To learn more&nbsp;<span className="font-bold underline cursor-pointer">click here</span>.
-      </p>
+    <div className="w-full h-10 bg-gradient-to-r from-orange-dark to-orange-light">
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,17 +4,17 @@ export default function Navbar() {
 
   return (
     <nav className="flex justify-between items-center h-16 bg-white text-gray-500 relative shadow-sm">
-      <Link to='/' className='pl-8 flex-shrink-0'>
+      <a href="https://foxit.com" target="_blank" rel="noreferrer" className='pl-8 flex-shrink-0'>
         <img src="/foxit-header-logo.png" alt="" className="h-10"/>
-      </Link>
-      <div className="md:block hidden flex-shrink-0">
-        <Link className="p-4 hover:bg-gray-100 rounded-md hover:text-blue-300 transition duration-100 ease-in-out" to='/'>Products</Link>
-        <Link className="p-4 hover:bg-gray-100 rounded-md hover:text-blue-300 transition duration-100 ease-in-out" to='/'>Store</Link>
-        <Link className="p-4 hover:bg-gray-100 rounded-md hover:text-blue-300 transition duration-100 ease-in-out" to='/'>Company</Link>
-        <Link className="p-4 hover:bg-gray-100 rounded-md hover:text-blue-300 transition duration-100 ease-in-out" to='/'>Support</Link>
-        <Link className="p-4 hover:bg-gray-100 rounded-md hover:text-blue-300 transition duration-100 ease-in-out" to='/'>Volume Licensing</Link>
-      </div>
-      <div className="md:block hidden flex-shrink-0">
+      </a>
+      {/*<div className="md:block hidden flex-shrink-0">*/}
+      {/*  <Link className="p-4 hover:bg-gray-100 rounded-md hover:text-blue-300 transition duration-100 ease-in-out" to='/'>Products</Link>*/}
+      {/*  <Link className="p-4 hover:bg-gray-100 rounded-md hover:text-blue-300 transition duration-100 ease-in-out" to='/'>Store</Link>*/}
+      {/*  <Link className="p-4 hover:bg-gray-100 rounded-md hover:text-blue-300 transition duration-100 ease-in-out" to='/'>Company</Link>*/}
+      {/*  <Link className="p-4 hover:bg-gray-100 rounded-md hover:text-blue-300 transition duration-100 ease-in-out" to='/'>Support</Link>*/}
+      {/*  <Link className="p-4 hover:bg-gray-100 rounded-md hover:text-blue-300 transition duration-100 ease-in-out" to='/'>Volume Licensing</Link>*/}
+      {/*</div>*/}
+      <div className="md:block flex-shrink-0">
         <Link className="p-4 w-32" to='/'>
           <span className="bg-blue-500 text-white rounded-md py-1 px-6">
             Free Trial
@@ -24,11 +24,11 @@ export default function Navbar() {
           </span>
         </Link>
       </div>
-      <div className="px-4 cursor-pointer md:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-        </svg>
-      </div>
+      {/*<div className="px-4 cursor-pointer md:hidden">*/}
+      {/*  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">*/}
+      {/*    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />*/}
+      {/*  </svg>*/}
+      {/*</div>*/}
     </nav>
   )
  }

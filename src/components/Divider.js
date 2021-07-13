7b50bb2e70486da5d0ae110acd20d1aeb7ce09f5
Junyu Zhu <alexzhuisme@gmail.com>
2021-07-13 11:55:32 -0700

    responsive update

--- src/components/Divider.js
+++ src/components/Divider.js
@@ -1,5 +1,5 @@
 export default function Divider(){
   return (
-    <div className="h-1 md:h-2 w-9/12 md:w-10/12 bg-white mx-auto mb-5 mt-5"/>
+    <div className="h-1 md:h-2 w-9/12 md:w-10/12 lg:w-5/12 bg-white mx-auto mb-5 mt-5"/>
   )
 }
